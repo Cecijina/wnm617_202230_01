503d5fea0cdcb75e5337ce5cc22b970a065d59f7
Cecilia Kim <ceciliakim@Cecilias-MacBook-Pro.local>
2022-05-09 00:31:07 -0700

    push everything again

---
+++ cecilia.kim/js/form.js
@@ -0,0 +1,17 @@
+ 
+
+
+const submitAnimalAdd = () => {
+   let name = $("#animal-add-name").val();
+   let type = $("#animal-add-type").val();
+   let breed = $("#animal-add-breed").val();
+   let description = $("#animal-add-description").val();
+
+   console.log({name,type,breed,description});
+
+   let{result,error} = await query ({
+   	type: 'insert_aniaml',
+   	
+   })
+}
+
